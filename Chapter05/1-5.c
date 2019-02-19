c89317582562b400e1f10b536256e4fe9cf6dec0
Divya Rachel <divyadeepti2000@gmail.com>
2019-02-20 00:38:05 +0530

    OK...here goes another

---
+++ Chapter05/1-5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    float fahr,cels;
+    int lower,upper,step;
+    lower=0;upper=300;step=20;
+    fahr=upper;
+    /*the heading thing*/
+    printf("Fahr\tCels\n");
+    while(fahr>=lower){
+        cels=(5.0/9.0)*(fahr-32.0);
+        printf("%3.0f \t %6.1f \n",fahr,cels);
+        fahr=fahr-step;
+    }
+}
